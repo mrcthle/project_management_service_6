009cdb536d5d504540b1a188687db2af32fd3d07
mrcthle <mirco.seghorn@gmail.com>
2022-10-13 09:14:43 +0200

    PMS6-28 moves HelloController class to controllers package

--- src/main/java/de/szut/lf8_project/controllers/HelloController.java
+++ src/main/java/de/szut/lf8_project/controllers/HelloController.java
@@ -1,8 +1,11 @@
-package de.szut.lf8_project.hello;
+package de.szut.lf8_project.controllers;
 
 import de.szut.lf8_project.exceptionHandling.ResourceNotFoundException;
-import de.szut.lf8_project.hello.dto.HelloCreateDto;
-import de.szut.lf8_project.hello.dto.HelloGetDto;
+import de.szut.lf8_project.dtos.helloDtos.HelloCreateDto;
+import de.szut.lf8_project.dtos.helloDtos.HelloGetDto;
+import de.szut.lf8_project.entities.HelloEntity;
+import de.szut.lf8_project.mappers.HelloMapper;
+import de.szut.lf8_project.services.HelloService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
