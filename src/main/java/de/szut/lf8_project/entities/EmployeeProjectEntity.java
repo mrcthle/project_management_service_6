672657da4daa700223da8093cd21ea5a32218999
mrcthle <mirco.seghorn@gmail.com>
2022-10-16 17:08:37 +0200

    PMS6-62 Creates entity class for mapping of employees and projects

---
+++ src/main/java/de/szut/lf8_project/entities/EmployeeProjectEntity.java
@@ -0,0 +1,28 @@
+package de.szut.lf8_project.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employee_project")
+public class EmployeeProjectEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "employee_id", nullable = false)
+    private Long employeeId;
+    
+    @Column(name = "project_id", nullable = false)
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.DETACH //Todo: Cascade Type überprüfen
+    )
+    private ProjectEntity projectEntity;
+    
+    @Column(name = "is_project_leader", nullable = false)
+    private boolean isProjectLeader;
+}
