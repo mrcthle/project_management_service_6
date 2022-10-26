9b9f3d86485f27cadbe0ffa20a970a7fef62e11b
mrcthle <mirco.seghorn@gmail.com>
2022-10-26 16:32:58 +0200

    PMS6-24 replace @Data with @AllArgsConstructor, @Getter and @Setter

--- src/main/java/de/szut/lf8_project/dtos/projectDto/GetProjectDTO.java
+++ src/main/java/de/szut/lf8_project/dtos/projectDto/GetProjectDTO.java
@@ -1,12 +1,16 @@
 package de.szut.lf8_project.dtos.projectDto;
 
 import de.szut.lf8_project.entities.EmployeeProjectEntity;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class GetProjectDTO {
     
     private Long id;
