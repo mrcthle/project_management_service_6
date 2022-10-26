4f23543c0d4550edcf264568f38df2e4e3199b4b
mrcthle <mirco.seghorn@gmail.com>
2022-10-26 20:43:25 +0200

    PMS6-27 adds validation of fields in create method

--- src/main/java/de/szut/lf8_project/services/ProjectService.java
+++ src/main/java/de/szut/lf8_project/services/ProjectService.java
@@ -1,23 +1,40 @@
 package de.szut.lf8_project.services;
 
+import de.szut.lf8_project.entities.EmployeeProjectEntity;
 import de.szut.lf8_project.entities.ProjectEntity;
 import de.szut.lf8_project.repositories.ProjectRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProjectService {
     
-    private ProjectRepository repository;
+    private final ProjectRepository repository;
+    private final  CustomerService customerService;
+    private final EmployeeService employeeService;
+    private final EmployeeProjectService employeeProjectService;
     
-    public ProjectService(ProjectRepository repository) {
+    public ProjectService(ProjectRepository repository, CustomerService customerService, EmployeeProjectService employeeProjectService) {
         this.repository = repository;
     }
     
     public ProjectEntity create(ProjectEntity projectEntity) {
-        return repository.save(projectEntity);
+        customerService.getCustomerById(projectEntity.getCustomerId());
+        employeeService.getEmployee(projectEntity.getProjectLeader());
+        
+        Set<EmployeeProjectEntity> employeeProjectEntities = projectEntity.getProjectEmployees();
+        for (EmployeeProjectEntity employeeProjectEntity : employeeProjectEntities) {
+            employeeService.getEmployee(employeeProjectEntity.getEmployeeId());
+            //Todo: check if employee has needed qualification
+        }
+        projectEntity = repository.save(projectEntity);
+        for (EmployeeProjectEntity employeeProjectEntity : projectEntity.getProjectEmployees()) {
+            employeeProjectService.create(employeeProjectEntity);
+        }
+        return projectEntity;
     }
     
     public ProjectEntity readById(Long id) {
