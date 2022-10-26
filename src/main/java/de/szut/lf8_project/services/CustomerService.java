a0a619a520e687c04be62a60869148126c4e453b
mrcthle <mirco.seghorn@gmail.com>
2022-10-26 17:15:51 +0200

    PMS6-39 adds class to return customers
    The implementation has to be replaced with an actual api call, but returns dummy data till then

---
+++ src/main/java/de/szut/lf8_project/services/CustomerService.java
@@ -0,0 +1,23 @@
+package de.szut.lf8_project.services;
+
+import de.szut.lf8_project.dtos.customerDto.CustomerDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerService {
+    
+    private final static CustomerDTO[] customerList = new CustomerDTO[]{
+            new CustomerDTO(1L, "Amazon", "Nelson Mandela"),
+            new CustomerDTO(2L, "Nestle", "Martin Luther King"),
+            new CustomerDTO(3L, "Facebook", "Edward Snowden")
+    };
+    
+    public List<CustomerDTO> getCustomers() {
+        return new ArrayList<>(List.of(customerList));
+    }
+    
+    public CustomerDTO getCustomerById(Long id) {
+        return (id > 3 || id < 1) ? null : customerList[(int) (id - 1)];
+    }
+}
