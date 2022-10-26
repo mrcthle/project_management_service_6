e0e81582d926b1d5206aa17cf078a0d183f1f83d
mrcthle <mirco.seghorn@gmail.com>
2022-10-26 20:50:12 +0200

    PMS6-24 changes field for employees

--- src/main/java/de/szut/lf8_project/dtos/projectDto/GetProjectDTO.java
+++ src/main/java/de/szut/lf8_project/dtos/projectDto/GetProjectDTO.java
@@ -1,12 +1,12 @@
 package de.szut.lf8_project.dtos.projectDto;
 
-import de.szut.lf8_project.entities.EmployeeProjectEntity;
+import de.szut.lf8_project.dtos.employeeDto.EmployeeDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,5 +20,5 @@ public class GetProjectDTO {
     private LocalDateTime startDate;
     private LocalDateTime plannedEndDate;
     private LocalDateTime endDate;
-    private Set<EmployeeProjectEntity> projectEmployees;
+    private List<EmployeeDTO> projectEmployees;
 }
