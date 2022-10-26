710992345726866723e60809674c83a90b4cc7c8
mrcthle <mirco.seghorn@gmail.com>
2022-10-26 20:38:31 +0200

    PMS6-24 removes unused services classes

--- src/main/java/de/szut/lf8_project/controllers/ProjectController.java
+++ src/main/java/de/szut/lf8_project/controllers/ProjectController.java
@@ -4,9 +4,6 @@ import de.szut.lf8_project.dtos.projectDto.AddProjectDTO;
 import de.szut.lf8_project.dtos.projectDto.GetProjectDTO;
 import de.szut.lf8_project.entities.ProjectEntity;
 import de.szut.lf8_project.mappers.ProjectMapper;
-import de.szut.lf8_project.services.CustomerService;
-import de.szut.lf8_project.services.EmployeeProjectService;
-import de.szut.lf8_project.services.EmployeeService;
 import de.szut.lf8_project.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("v1/api/pms/project")
 public class ProjectController {
-
-    private final CustomerService customerService;
-    private final EmployeeProjectService employeeProjectService;
-    private final EmployeeService employeeService;
+    
     private final ProjectMapper projectMapper;
     private final ProjectService projectService;
     
     public ProjectController(
-            CustomerService customerService, 
-            EmployeeProjectService employeeProjectService, 
-            EmployeeService employeeService, 
             ProjectMapper projectMapper,
             ProjectService projectService
     ) {
-        this.customerService = customerService;
-        this.employeeProjectService = employeeProjectService;
-        this.employeeService = employeeService;
         this.projectMapper = projectMapper;
         this.projectService = projectService;
     }
