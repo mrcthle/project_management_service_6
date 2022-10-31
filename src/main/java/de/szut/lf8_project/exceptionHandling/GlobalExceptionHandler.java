ab1008103f8ab0c0d54f80e85c2301d96dc8848d
mrcthle <mirco.seghorn@gmail.com>
2022-10-31 16:53:27 +0100

    PMS6-10 rename method

--- src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
+++ src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
@@ -10,9 +10,9 @@ import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleHelloEntityNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    public ResponseEntity<?> handleNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
