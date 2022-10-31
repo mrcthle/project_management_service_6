b5ce031b1962ecba36e41d546d30fb7a32cec2cf
mrcthle <mirco.seghorn@gmail.com>
2022-10-31 15:54:49 +0100

    PMS6-10 adds exception class

---
+++ src/main/java/de/szut/lf8_project/exceptionHandling/SkillSetNotFound.java
@@ -0,0 +1,8 @@
+package de.szut.lf8_project.exceptionHandling;
+
+public class SkillSetNotFound extends RuntimeException {
+    
+    public SkillSetNotFound(String message) {
+        super(message);
+    }
+}
