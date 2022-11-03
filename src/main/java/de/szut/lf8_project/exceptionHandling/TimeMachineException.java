10f611b4187b2c217e382af16c15990244ba32c5
mrcthle <mirco.seghorn@gmail.com>
2022-11-03 09:21:45 +0100

    PMS6-10 adds two Exception classes

---
+++ src/main/java/de/szut/lf8_project/exceptionHandling/TimeMachineException.java
@@ -0,0 +1,8 @@
+package de.szut.lf8_project.exceptionHandling;
+
+public class TimeMachineException extends RuntimeException {
+    
+    public TimeMachineException(String message) {
+        super(message);
+    }
+}
