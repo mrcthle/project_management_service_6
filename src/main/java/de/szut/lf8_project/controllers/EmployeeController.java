b00db4d8beefcb93262d95792b55dd44f4fa69e9
mrcthle <mirco.seghorn@gmail.com>
2022-11-03 10:57:49 +0100

    PMS6-53 creates controller with read method

---
+++ src/main/java/de/szut/lf8_project/controllers/EmployeeController.java
@@ -0,0 +1,50 @@
+package de.szut.lf8_project.controllers;
+
+import de.szut.lf8_project.dtos.employeeDto.EmployeeDTO;
+import de.szut.lf8_project.dtos.projectDto.GetProjectDTO;
+import de.szut.lf8_project.entities.EmployeeProjectEntity;
+import de.szut.lf8_project.entities.ProjectEntity;
+import de.szut.lf8_project.mappers.ProjectMapper;
+import de.szut.lf8_project.services.EmployeeService;
+import de.szut.lf8_project.services.ProjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("v1/api/pms/employee")
+public class EmployeeController {
+    
+    private final EmployeeService employeeService;
+    private final ProjectMapper projectMapper;
+    private final ProjectService projectService;
+    
+    public EmployeeController(EmployeeService employeeService, ProjectMapper projectMapper, ProjectService projectService) {
+        this.employeeService = employeeService;
+        this.projectMapper = projectMapper;
+        this.projectService = projectService;
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<List<GetProjectDTO>> readAllProjectsByEmployeeId(@PathVariable Long id) {
+        EmployeeDTO employee = employeeService.getEmployee(id);
+        List<ProjectEntity> projectEntities = projectService.readAll();
+        List<GetProjectDTO> projectDTOs = new ArrayList<>();
+        for (ProjectEntity projectEntity : projectEntities) {
+            for (EmployeeProjectEntity employeeProjectEntity : projectEntity.getProjectEmployees()) {
+                if (Objects.equals(employeeProjectEntity.getEmployeeId(), employee.getId())) {
+                    projectDTOs.add(projectMapper.mapToGetDto(projectEntity));
+                    break;
+                }
+            }
+        }
+        return new ResponseEntity<>(projectDTOs, HttpStatus.OK);
+    }
+}
