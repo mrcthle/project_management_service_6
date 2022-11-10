3632eb76795dfdcb74e70a3feb8b423fc21358f3
mrcthle <mirco.seghorn@gmail.com>
2022-11-10 09:26:58 +0100

    #integration-tests adds new Exception

---
+++ src/main/java/de/szut/lf8_project/exceptionHandling/SkillSetNotNeededException.java
@@ -0,0 +1,8 @@
+package de.szut.lf8_project.exceptionHandling;
+
+public class SkillSetNotNeededException extends RuntimeException {
+    
+    public SkillSetNotNeededException(String message) {
+        super(message);
+    }
+}
