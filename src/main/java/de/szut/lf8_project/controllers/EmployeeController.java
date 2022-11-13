fa45817ec7aaab9cd356a288541eeddee3abfa44
mrcthle <mirco.seghorn@gmail.com>
2022-11-13 16:19:22 +0100

    #documentation adds documentation for parameters

--- src/main/java/de/szut/lf8_project/controllers/EmployeeController.java
+++ src/main/java/de/szut/lf8_project/controllers/EmployeeController.java
@@ -7,13 +7,7 @@ import de.szut.lf8_project.entities.ProjectEntity;
 import de.szut.lf8_project.mappers.ProjectMapper;
 import de.szut.lf8_project.services.EmployeeService;
 import de.szut.lf8_project.services.ProjectService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,7 +21,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("v1/api/pms/employee")
-public class EmployeeController {
+public class EmployeeController extends AbstractEmployeeController {
     
     private final EmployeeService employeeService;
     private final ProjectMapper projectMapper;
@@ -38,35 +32,11 @@ public class EmployeeController {
         this.projectMapper = projectMapper;
         this.projectService = projectService;
     }
-    @Operation(summary = "returns projects by employee id", description = "The endpoint returns a list of all projects an employee is part of.")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200", 
-                    description = "Read all projects by employee id.",
-                    content = {@Content(
-                                mediaType = "application/json", 
-                                array = @ArraySchema(schema = @Schema(implementation = GetProjectDTO.class))
-                    )}
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "The validation of one or more fields failed.",
-                    content = @Content
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "You are not authorized.",
-                    content = @Content
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "An employee with this id could not be found.",
-                    content = @Content
-            )
-    })
+    
+    @Override
     @GetMapping("/{id}")
     public ResponseEntity<List<GetProjectDTO>> readAllProjectsByEmployeeId(
-            @Parameter(description = "id of employee")
+            @Parameter(description = "id of employee", required = true)
             @PathVariable Long id
     ) {
         EmployeeDTO employee = employeeService.getEmployee(id);
