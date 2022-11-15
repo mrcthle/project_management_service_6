20b77a423dbeb3b7c093fe1ccbfe9549d9454019
mrcthle <mirco.seghorn@gmail.com>
2022-11-15 06:11:54 +0100

    #refactoring adds repository annotation to repository interfaces

--- src/main/java/de/szut/lf8_project/repositories/ProjectQualificationRepository.java
+++ src/main/java/de/szut/lf8_project/repositories/ProjectQualificationRepository.java
@@ -3,9 +3,11 @@ package de.szut.lf8_project.repositories;
 import de.szut.lf8_project.entities.ProjectEntity;
 import de.szut.lf8_project.entities.ProjectQualificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProjectQualificationRepository extends JpaRepository<ProjectQualificationEntity, Long> {
     
     List<ProjectQualificationEntity> findAllByProjectEntity(ProjectEntity projectEntity);
--- src/main/java/de/szut/lf8_project/repositories/ProjectRepository.java
+++ src/main/java/de/szut/lf8_project/repositories/ProjectRepository.java
@@ -2,6 +2,8 @@ package de.szut.lf8_project.repositories;
 
 import de.szut.lf8_project.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 }
